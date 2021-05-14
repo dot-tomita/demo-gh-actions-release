b7b9da9c1472864aecdd192c79da0b9c3e85267a
yuki.tomita <yuki.tomita@d2cdot.co.jp>
2021-05-14 21:20:18 +0900

    add: テスト用のnextプロジェクトを追加

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import styles from '../styles/Home.module.css';
+
+export async function getStaticProps(context) {
+  const hoge = 'Hello!';
+  return {
+    props: {
+      hoge,
+    },
+  };
+}
+
+export default function Home({ hoge }) {
+  return (
+    <>
+      <div className="container">
+        <h1>{hoge} world!!</h1>
+      </div>
+    </>
+  );
+}
