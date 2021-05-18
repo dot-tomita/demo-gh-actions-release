99cc0988133b117edafc529d700390f61d0583db
yuki.tomita <yuki.tomita@d2cdot.co.jp>
2021-05-18 16:25:21 +0900

    マージしても反映されないはず

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ hoge }) {
       <div className="container">
         <h1>{hoge} world!!</h1>
         <p>こんにちは！！！</p>
-        <p>update</p>
+        <p>このpushはmasterにマージされてもworkflowは実行されないはず</p>
       </div>
     </>
   );
